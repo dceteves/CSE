3caa0f7074cc4c0040302d2986fc9f42c90007a6
luvouva (Devin) <dceteves@gmail.com>
2017-11-29 09:46:57 -0800

    Notes

--- Notes.py
+++ Notes.py
@@ -1,51 +1,64 @@
-# Devin
+'''Devin
 
-# a = 4
-# b = 3
+a = 4
+b = 3
 
-# print(3 + 5)
-# print(5 -3)
-# print(3 * 5)
-# print(6 / 2)
-# print(3 ** 2)
+print(3 + 5)
+print(5 -3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
 
-# print("Try to figure out how this works")
-# print(13 % 12)
+print("Try to figure out how this works")
+print(13 % 12)
 
-# car_name = "Wiebe Mobile"
-# car_type = "BMW"
-# car_cylinders = 8
-# car_mpg = 5000.9
+car_name = "Wiebe Mobile"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
 
-# print("I have a car called %s. It's pretty nice." % car_name)
-# print("I have a car called %s. It's a %s." % (car_name, car_type))
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type))
                                               # Watch the order!
 
-# print("What is your name?")
-# name = input(">_ ")
-# print("Hello %s" % name)
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s" % name)
 
-# age = input("How old are you?")
+age = input("How old are you?")
 
-# print("%s?! That's really old. You belong in a retirement hom." % age)
+print("%s?! That's really old. You belong in a retirement hom." % age)
 
 def print_hw():
     print("Hello World.")
     print("Enjoy the day.")
 
+
+
 def say_hi(name):
     print("Hello %s" % name)
     print("Coding is great!")
-say_hi("Devin")
+
+
 
 def print_age(name, age):
     print("%s is %d years old" % (name, age))
     age = age + 1
     print("Next year, %s will be %d years old" % (name, age)
 
-# print_age("Devin", 14)
-
 def algebra_hw(x):
-    return x**3 + 4+x++2 + 7 * x - 4
+    return x ** 3 + 4 + x + +2 + 7 * x - 4
+'''
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    elif percentage >= 50:
+        return "F"
 
-algebra_hw(3)
+print(grade_calc(90))
