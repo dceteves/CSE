a5f9804b2e78d56ff93625c145ac0ea67480cb74
luvouva (Devin) <dceteves@gmail.com>
2017-11-29 09:37:46 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,51 @@
+# Devin
+
+# a = 4
+# b = 3
+
+# print(3 + 5)
+# print(5 -3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+
+# print("Try to figure out how this works")
+# print(13 % 12)
+
+# car_name = "Wiebe Mobile"
+# car_type = "BMW"
+# car_cylinders = 8
+# car_mpg = 5000.9
+
+# print("I have a car called %s. It's pretty nice." % car_name)
+# print("I have a car called %s. It's a %s." % (car_name, car_type))
+                                              # Watch the order!
+
+# print("What is your name?")
+# name = input(">_ ")
+# print("Hello %s" % name)
+
+# age = input("How old are you?")
+
+# print("%s?! That's really old. You belong in a retirement hom." % age)
+
+def print_hw():
+    print("Hello World.")
+    print("Enjoy the day.")
+
+def say_hi(name):
+    print("Hello %s" % name)
+    print("Coding is great!")
+say_hi("Devin")
+
+def print_age(name, age):
+    print("%s is %d years old" % (name, age))
+    age = age + 1
+    print("Next year, %s will be %d years old" % (name, age)
+
+# print_age("Devin", 14)
+
+def algebra_hw(x):
+    return x**3 + 4+x++2 + 7 * x - 4
+
+algebra_hw(3)
