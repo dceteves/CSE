ae317b2c9660fe8750a17f52d3449bef023d070d
luvouva <dceteves@gmail.com>
2018-01-17 21:19:44 -0800

    Lucky 7's Updated
    Updated Lucky 7's to tell you the highest amount of money you earned.
    
    1/17/18

--- Devin Teves - Lucky 7's.py
+++ Devin Teves - Lucky 7's.py
@@ -1,6 +1,7 @@
 from random import randint
 rounds = 0
 money = 15
+highest = 0
 while money != 0:
     die, die2 = randint(0, 6), randint(0, 6)
     rounds += 1
@@ -8,7 +9,11 @@ while money != 0:
     if die + die2 == 7:
         money += 4
         print("You won money! \nYou have $%s. \n" % money)
-    else:
+    elif die + die2 != 7:
         money -= 1
         print("You lost money! You rolled %s. \nYou have $%s. \n" % (die + die2, money))
-print("You ran out of money! It took you %s rounds until you lost all of your money. \nGame over." % rounds)
+    if money > highest:
+        highest = money
+    print(highest)
+print("You ran out of money! It took you %s rounds until you lost all of your money. "
+      "\nYour highest was $%s.\nGame over." % (rounds, highest))
